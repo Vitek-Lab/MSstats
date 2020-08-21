d85bb064a8748a4ba0cdbc5302f2d75c4438d2e7
Mateusz Staniak <mtst@mstaniak.pl>
2020-08-21 15:02:50 +0200

    adds function to filter single-feature proteins

---
+++ R/utils_filter.R
@@ -0,0 +1,13 @@
+#' @importFrom data.table uniqueN
+.removeSingleLabelFeatures = function(input) {
+    if (data.table::uniqueN(input$LABEL) == 2) {
+        counts = na.omit(input)[, list(n_obs = .N), 
+                                by = c("LABEL", "FEATURE", 
+                                       "GROUP_ORIGINAL")]
+        counts[, AnyMissing := any(n_obs == 0), by = c("FEATURE", "LABEL")]
+        counts = counts[(AnyMissing), ]
+        input$SuggestToFilter = ifelse(input$FEATURE %in% unique(counts$FEATURE),
+                                       1, 0)
+    }
+    input 
+}
