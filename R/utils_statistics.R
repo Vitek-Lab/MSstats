b54b8251981208c7cf25a15c07d0a1f8119c79d5
Mateusz Staniak <mtst@mstaniak.pl>
2020-08-21 15:02:31 +0200

    adds functions to calculate dataset summaries

---
+++ R/utils_statistics.R
@@ -0,0 +1,119 @@
+.logDatasetInformation = function(input) {
+    .logSummaryStatistics(input)
+    .checkSingleLabelProteins(input)
+    .logMissingness(input)
+}
+
+.logSummaryStatistics = function(input) {
+    num_proteins = data.table::uniqueN(input$PROTEIN)
+    peptides_per_protein = input[, list(peptide_count = data.table::uniqueN(PEPTIDE)),
+                                 by = "PROTEIN"]
+    features_per_peptide = input[, list(feature_count = data.table::uniqueN(FEATURE)),
+                                 by = "PEPTIDE"]
+    features_per_protein = input[, list(feature_count = data.table::uniqueN(FEATURE)),
+                                 by = "PROTEIN"]
+    features_per_protein = features_per_protein[feature_count == 1L, PROTEIN]
+    
+    pep_per_prot = range(peptides_per_protein$peptide_count)
+    feat_per_pept = range(features_per_peptide$feature_count)
+    counts_msg = paste("", paste("# proteins:", num_proteins),
+                       paste("# peptides per protein:", 
+                             paste(pep_per_prot, sep = "-", collapse = "-")),
+                       paste("# features per peptide:",
+                             paste(feat_per_pept, sep = "-", collapse = "-")),
+                       sep = "\n", collapse = "\n")
+    getOption("MSstatsMsg")("INFO", counts_msg)
+    getOption("MSstatsLog")("INFO", counts_msg)
+    
+    if (length(features_per_protein) > 0) {
+        single_msg = paste("The following proteins have only one feature:", "\n",
+                           paste(unique(as.character(features_per_protein)),
+                                 sep = ",\n ", collapse = ",\n"))
+        getOption("MSstatsLog")("INFO", single_msg)
+        getOption("MSstatsMsg")("INFO", single_msg)
+    }
+
+    samples_info = input[, list(NumRuns = data.table::uniqueN(RUN),
+                                NumBioReplicates = data.table::uniqueN(SUBJECT_ORIGINAL),
+                                NumFractions = data.table::uniqueN(FRACTION)), 
+                         by = "GROUP_ORIGINAL"]
+    samples_info = samples_info[, list(GROUP_ORIGINAL, NumRuns, NumBioReplicates, 
+                                       NumTechReplicates = as.integer(round(NumRuns / (NumBioReplicates * NumFractions))))]
+    samples_info = data.table::dcast(data.table::melt(samples_info, id.vars = "GROUP_ORIGINAL"), variable ~ GROUP_ORIGINAL)
+    colnames(samples_info)[1] = ""
+    samples_info[, 1] = c("# runs", "# bioreplicates", "# tech. replicates")
+    
+    samples_info = rbind(data.table::data.table(t(colnames(samples_info))), 
+                         samples_info, use.names = FALSE)
+    samples_msg = apply(samples_info, 2, .nicePrint)
+    samples_msg = apply(samples_msg, 1, function(x) paste0(x, collapse = ""))
+    samples_msg = paste("", samples_msg, sep = "\n", collapse = "\n")
+    getOption("MSstatsLog")("INFO", samples_msg)
+    getOption("MSstatsMsg")("INFO", samples_msg)    
+}
+
+.nicePrint = function(string_vector) {
+    max_chars = max(nchar(string_vector))   
+    string_vector = sapply(string_vector, 
+                           function(x) paste(paste0(rep(" ", max_chars - nchar(x)), collapse = ""), x),
+                           USE.NAMES = FALSE)
+} 
+
+.logSingleLabeledProteins = function(input, label) {
+    name = ifelse(label == "L", "endogeneous", "reference")
+    proteins = input[LABEL == label, as.character(PROTEIN)]
+    msg = paste(paste("The following proteins only have", name,
+                        "intensities in label-based experiment",
+                        "Please check or remove these proteins:"),
+                  paste(proteins, sep = ", \n ", collapse = ", \n "))
+    if (length(proteins) > 0) {
+        getOption("MSstatsMsg")("WARN", msg)
+        getOption("MsstatsLog")("WARN", msg)
+    }
+    
+}
+
+.checkSingleLabelProteins = function(input) {
+    if (data.table::uniqueN(input$LABEL) == 2) {
+        labels_by_protein = unique(input[, list(LABEL, label_count = data.table::uniqueN(LABEL)),
+                                         by = "PROTEIN"])
+        labels_by_protein = labels_by_protein[label_count == 1L, ]
+        
+        .logSingleLabeledProteins(labels_by_protein, "L")
+        .logSingleLabeledProteins(labels_by_protein, "H")
+    }
+}
+
+
+.logMissingness = function(input) {
+    missing = input[, list(NumMissing = sum(is.na(ABUNDANCE), na.rm = TRUE),
+                           NumTotal = .N), 
+                    by = c("LABEL", "GROUP_ORIGINAL", "FEATURE")]
+    missing[, AllMissing := NumMissing == NumTotal]
+    missing[, AnyAllMissing := any(AllMissing), by = c("LABEL", "FEATURE")]
+    missing_in_any = as.character(missing[(AnyAllMissing), FEATURE])
+    # LOG, which features have AnyAllMissing
+    
+    missing_by_run = input[, list(NumMissing = sum(is.na(ABUNDANCE), na.rm = TRUE),
+                                  NumTotal = .N), by = "RUN"]
+    missing_by_run[, FractionMissing := NumMissing / NumTotal]
+    missing_by_run = as.character(missing_by_run[FractionMissing > 0.75, RUN])
+    
+    if (length(missing_in_any) > 0) {
+        msg = paste("The following features are completely missing in at least one condition,\n",
+                    paste(missing_in_any, sep = ",\n ", collapse = ",\n "))
+        msg_short = paste("Some feature are completely missing in at least one condition, for example:\n",
+                          paste(missing_in_any[1:5], sep = ",\n ", collapse = ",\n "),
+                          "(See the log for a complete list).")
+        getOption("MSstatsLog")("INFO", msg)
+        getOption("MSstatsMsg")("INFO", msg_short)
+    }
+    
+    if (length(missing_by_run) > 0) {
+        msg = paste("The following runs have more than 75% missing values:",
+                    paste(missing_by_run, sep = ",\n ", collapse = ",\n "))
+        getOption("MSstatsLog")("INFO", msg)
+        getOption("MSstatsMsg")("INFO", msg)
+    }
+}
+
