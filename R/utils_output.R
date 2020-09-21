767674b0aedcbdc18124457fa7585001d6ce129e
Mateusz Staniak <mtst@mstaniak.pl>
2020-09-21 03:48:42 +0200

    renames output function

--- R/utils_output.R
+++ R/utils_output.R
@@ -1,5 +1,16 @@
-.processFinalOutput = function(input, rqresult, summary_method) {
-    if (inherits(rqresult, "try-error")) {
+#' Post-processing output from MSstats summarization
+#' 
+#' @param input `data.table` in MSstats format
+#' @param summarized output of the `MSstatsSummarize` function
+#' @param summary_method name of the summarization method
+#' (`summaryMethod` parameter to `dataProcess`)
+#' 
+#' @export
+#' 
+MSstatsSummarizationOutput = function(input, summarized, summary_method) {
+    GROUP = Protein = RUN = NULL
+    
+    if (inherits(summarized, "try-error")) {
         msg = paste("*** error : can't summarize per subplot with ", 
                     summary_method, ".")
         getOption("MSstatsLog")("ERROR", msg)
@@ -9,30 +20,27 @@
         workpred = NULL
     } else {
         label <- nlevels(input$LABEL) == 2
-        if (!is.element("RUN", colnames(rqresult))) {
+        if (!is.element("RUN", colnames(summarized))) {
             lab = unique(input[, c("GROUP", "GROUP_ORIGINAL", "SUBJECT_ORIGINAL", "SUBJECT_NESTED", "SUBJECT")])
             if (label) {
                 lab = lab[GROUP != 0, ]
             }
-            rqall = merge(rqresult, lab, by = "SUBJECT_ORIGINAL")
+            rqall = merge(summarized, lab, by = "SUBJECT_ORIGINAL")
         } else {
             lab = unique(input[, c("RUN", "originalRUN", "GROUP", "GROUP_ORIGINAL", 
                                    "SUBJECT_ORIGINAL", "SUBJECT_NESTED", "SUBJECT")])
             if (label) {
-                lab = lab[lab$GROUP != 0, ]
+                lab = lab[GROUP != 0, ]
             }
-            rqall = merge(rqresult, lab, by = "RUN")
+            rqall = merge(summarized, lab, by = "RUN")
         }
         
         rqall$GROUP = factor(rqall$GROUP)
         rqall$Protein = factor(rqall$Protein)
-        rqmodelqc = rqresult$ModelQC
+        rqmodelqc = summarized$ModelQC
         #MC : can't use this predicted value.
         #workpred <- rqresult$PredictedBySurvival
         workpred <- NULL
-        msg = " == the summarization per subplot is done."
-        getOption("MSstatsLog")("INFO", msg)
-        getOption("MSstatsMsg")("INFO", msg)
     }
     
     if (is.element("RUN", colnames(rqall))) {
