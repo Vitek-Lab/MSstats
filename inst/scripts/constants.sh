866bc355af6205acb38b2335ff6036dfc9e03720
Ajeya Kempegowda <ajeyakempegowda@Ajeyas-MacBook-Pro.local>
2020-12-18 17:58:45 -0500

    removed commented code

--- inst/scripts/constants.sh
+++ inst/scripts/constants.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 # Package constants
-# source_path="/home/rstudio"
-# code_deploy_path="code/deployment/msstats-dev"
-# test_folder="tests"
-# test_utils_folder="utils"
-
-source_path="/Users/ajeyakempegowda/Documents/ajeya/R-projects"
-code_deploy_path="capstone/msstatsdev"
+source_path="/home/rstudio"
+code_deploy_path="code/deployment/msstats-dev"
 test_folder="tests"
 test_utils_folder="utils"
 #---------------------------------------------------------------#
