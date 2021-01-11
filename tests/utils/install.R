f1f8185778ec1a04d04369ac9507828676885395
Mateusz Staniak <mtst@mstaniak.pl>
2021-01-12 00:35:32 +0100

    updated install.R
    paths to R library folder

--- tests/utils/install.R
+++ tests/utils/install.R
@@ -13,12 +13,16 @@ if (!requireNamespace("xlsx", quietly = TRUE))
     install.packages("xlsx")
 
 update.packages(oldPkgs = old.packages(c("devtools", "paws", "xlsx", "BiocManager")),
-                ask = FALSE)
+                ask = FALSE,
+                lib.loc = "~/R/x86_64-pc-linux-gnu-library/4.0")
 
-BiocManager::install("MSstats", ask = FALSE)
+BiocManager::install("MSstats", ask = FALSE,
+                     lib = "~/R/x86_64-pc-linux-gnu-library/4.0")
 devtools::install_github("Vitek-Lab/MSstats-dev", ref = "develop", 
                          dependencies = NA, quiet = TRUE,
-                         upgrade = "always")
+                         upgrade = "always",
+                         lib = "~/R/x86_64-pc-linux-gnu-library/4.0")
 devtools::install_github("Vitek-Lab/MSstatsTMT", ref = "develop", 
                          dependencies = NA, quiet = TRUE,
-                         upgrade = "always")
+                         upgrade = "always",
+                         lib = "~/R/x86_64-pc-linux-gnu-library/4.0")
