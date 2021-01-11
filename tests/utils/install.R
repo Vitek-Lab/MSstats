1aec5e639b5731a084eccea0898e4b8ac12249ed
Mateusz Staniak <mtst@mstaniak.pl>
2021-01-11 21:36:10 +0100

    updated after_install.sh and install.R
    to check for major packages that need update

--- tests/utils/install.R
+++ tests/utils/install.R
@@ -12,6 +12,9 @@ if (!requireNamespace("paws", quietly = TRUE))
 if (!requireNamespace("xlsx", quietly = TRUE))
     install.packages("xlsx")
 
+update.packages(oldPkgs = old.packages(c("devtools", "paws", "xlsx", "BiocManager")),
+                ask = FALSE)
+
 BiocManager::install("MSstats", ask = FALSE)
 devtools::install_github("Vitek-Lab/MSstats-dev", ref = "develop", 
                          dependencies = NA, quiet = TRUE,
