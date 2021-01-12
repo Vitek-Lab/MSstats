7904ee58fff50b499654cec45769fd5feff0ecb0
Mateusz Staniak <mtst@mstaniak.pl>
2021-01-12 20:06:28 +0100

    updated dataprocess.R
    to track results (debuggin)

--- tests/dataprocess.R
+++ tests/dataprocess.R
@@ -303,7 +303,7 @@ run_wider_testing <- function(metadata,
 
 exceptions <- data.frame()
 result_df <-run_wider_testing(metadata_s3[1])
-
+print(head(result_df))
 # # check if exception occured
 # is_error <-  F
 # if (nrow(exceptions) != 0){
