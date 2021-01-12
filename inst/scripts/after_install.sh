093199bafd9a04831a00b83b2c0478f5b4abda71
Mateusz Staniak <mtst@mstaniak.pl>
2021-01-12 04:21:00 +0100

    updated after_install.sh and dataprocess.R

--- inst/scripts/after_install.sh
+++ inst/scripts/after_install.sh
@@ -14,6 +14,6 @@ yes a | Rscript ./utils/install.R
 sudo chmod +x dataprocess.R
 # & to run in background
 # pointing output to dev/null so that code deploy doesn't stall
-cmd_arg=$source_path/$code_deploy_path/$test_folder/$test_utils_folder
-# yes a | ./dataprocess.R ${cmd_arg} > /dev/null 2> /dev/null < /dev/null &
-tmux -vv new-session -d -s "msstatstest" Rscript ./dataprocess.R ${cmd_arg}
+# cmd_arg=$source_path/$code_deploy_path/$test_folder/$test_utils_folder
+yes a | Rscript ./dataprocess.R > /dev/null 2> /dev/null < /dev/null &
+# tmux -vv new-session -d -s "msstatstest" Rscript ./dataprocess.R
