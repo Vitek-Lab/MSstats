f93117708c677ba8a43f1280b180a7edf87d1487
Mateusz Staniak <mtst@mstaniak.pl>
2021-01-12 01:04:39 +0100

    updated install.R
    changed MSstats source

--- tests/utils/install.R
+++ tests/utils/install.R
@@ -12,12 +12,17 @@ if (!requireNamespace("paws", quietly = TRUE))
 if (!requireNamespace("xlsx", quietly = TRUE))
     install.packages("xlsx")
 
-update.packages(oldPkgs = old.packages(c("devtools", "paws", "xlsx", "BiocManager")),
-                ask = FALSE,
-                lib.loc = "~/R/x86_64-pc-linux-gnu-library/4.0")
+# update.packages(oldPkgs = old.packages(c("devtools", "paws", "xlsx", "BiocManager")),
+#                 ask = FALSE,
+#                 lib.loc = "~/R/x86_64-pc-linux-gnu-library/4.0")
 
-BiocManager::install("MSstats", ask = FALSE,
-                     lib = "~/R/x86_64-pc-linux-gnu-library/4.0")
+# BiocManager::install("MSstats", ask = FALSE,
+#                      lib = "~/R/x86_64-pc-linux-gnu-library/4.0")
+devtools::install_github("MeenaChoi/MSstats", ref = "master", 
+                         dependencies = NA, quiet = TRUE,
+                         upgrade = "always",
+                         lib = "~/R/x86_64-pc-linux-gnu-library/4.0")
+
 devtools::install_github("Vitek-Lab/MSstats-dev", ref = "develop", 
                          dependencies = NA, quiet = TRUE,
                          upgrade = "always",
