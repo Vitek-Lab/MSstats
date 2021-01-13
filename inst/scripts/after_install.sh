4b751b3306e525dfe30fd5c753ce14524b6e29c9
Mateusz Staniak <mtst@mstaniak.pl>
2021-01-13 02:50:26 +0100

    fixed a typo in after_install.sh

--- inst/scripts/after_install.sh
+++ inst/scripts/after_install.sh
@@ -15,7 +15,7 @@ sudo chmod +x dataprocess.R
 # & to run in background
 # pointing output to dev/null so that code deploy doesn't stall
 # cmd_arg=$source_path/$code_deploy_path/$test_folder/$test_utils_folder
-sudo chmod 777 /home/rstudio/code/deployment/mssstats-dev/tests
+sudo chmod 777 /home/rstudio/code/deployment/msstats-dev/tests/
 yes a | Rscript ./dataprocess.R |& tee /home/rstudio/code/deployment/logs/dataprocess.log > /dev/null 2> /dev/null < /dev/null &
 # 
 # sudo Rscript ./dataprocess.R |& tee /home/rstudio/code/deployment/logs/dataprocess.log
