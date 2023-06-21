591e5ff8986ee1644e18edbcf461d7ce1be797c2
devonjkohler <devonjkohler@gmail.com>
2023-06-21 16:07:35 -0400

    added fp converter

--- R/converters.R
+++ R/converters.R
@@ -636,7 +636,7 @@ PDtoMSstatsFormat = function(
 #' @inheritParams .documentFunction
 #' @param input name of MSstats input report from Skyline, which includes feature-level data.
 #' @param annotation name of 'annotation.txt' data which includes Condition, BioReplicate, Run. If annotation is already complete in Skyline, use annotation=NULL (default). It will use the annotation information from input.
-#' @param removeiRT TRUE (default) will remove the proteins or peptides which are labeld 'iRT' in 'StandardType' column. FALSE will keep them.
+#' @param removeiRT TRUE (default) will remove the proteins or peptides which are labeled 'iRT' in 'StandardType' column. FALSE will keep them.
 #' @param filter_with_Qvalue TRUE(default) will filter out the intensities that have greater than qvalue_cutoff in DetectionQValue column. Those intensities will be replaced with zero and will be considered as censored missing values for imputation purpose.
 #' @param qvalue_cutoff Cutoff for DetectionQValue. default is 0.01.
 #' @param ... additional parameters to `data.table::fread`.
@@ -799,6 +799,56 @@ SpectronauttoMSstatsFormat = function(
                                 summarize_multiple_psms = summaryforMultipleRows),
         score_filtering = list(pgq = pq_filter, 
                                psm_q = qval_filter),
+        columns_to_fill = list("IsotopeLabelType" = "L"))
+    input = MSstatsConvert::MSstatsBalancedDesign(input, feature_columns)
+    
+    msg_final = paste("** Finished preprocessing. The dataset is ready",
+                      "to be processed by the dataProcess function.")
+    getOption("MSstatsLog")("INFO", msg_final)
+    getOption("MSstatsMsg")("INFO", msg_final)
+    getOption("MSstatsLog")("INFO", "\n")
+    input
+}
+
+#' Import FragPipe files
+#' 
+#' @param input name of FragPipe msstats.csv export. ProteinName, PeptideSequence, PrecursorCharge, FragmentIon, ProductCharge, IsotopeLabelType, Condition, BioReplicate, Run, Intensity are required.
+#' @param ... additional parameters to `data.table::fread`.
+#' @inheritParams .documentFunction
+#' 
+#' @return data.frame in the MSstats required format.
+#' 
+#' @author Devon Kohler
+#' 
+#' @export
+#' 
+#' @examples 
+#' TODO
+#' 
+FragPipetoMSstatsFormat = function(
+        input, useUniquePeptide = TRUE, removeFewMeasurements=TRUE,
+        removeProtein_with1Feature = FALSE, summaryforMultipleRows = max,
+        use_log_file = TRUE, append = FALSE, verbose = TRUE, log_file_path = NULL,
+        ...
+) {
+    MSstatsConvert::MSstatsLogsSettings(use_log_file, append, verbose, 
+                                        log_file_path)
+    
+    input = MSstatsConvert::MSstatsImport(list(input = input), 
+                                          "MSstats", "FragPipe", ...)
+    input = MSstatsConvert::getInputFile(input, "input")
+    
+    annotation = MSstatsConvert::MSstatsMakeAnnotation(input, NULL)
+    
+    feature_columns = c("PeptideSequence", "PrecursorCharge", "FragmentIon", "ProductCharge")
+    input = MSstatsConvert::MSstatsPreprocess(
+        input, 
+        annotation, 
+        feature_columns,
+        remove_shared_peptides = useUniquePeptide,
+        remove_single_feature_proteins = removeProtein_with1Feature,
+        feature_cleaning = list(remove_features_with_few_measurements = removeFewMeasurements,
+                                summarize_multiple_psms = summaryforMultipleRows),
         columns_to_fill = list("IsotopeLabelType" = "L"))
     input = MSstatsConvert::MSstatsBalancedDesign(input, feature_columns)
     
