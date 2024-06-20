4480bbf429200187db8adfb6454e59ac3b0ba2de
Tony Wu <ant.wu@northeastern.edu>
2024-06-20 19:07:46 -0400

    Add integration to INDRA html assembler link

--- R/networkPlots.R
+++ R/networkPlots.R
@@ -4,6 +4,7 @@
 #' @param input list of hgnc IDs
 #' @importFrom jsonlite toJSON
 #' @importFrom httr POST, add_headers
+#' @importFrom r2r hashmap, query
 #' 
 #' @export
 #' 
@@ -13,7 +14,11 @@
 visualizeNetworks = function(input) {
   # Fetch data from INDRA
   input = c("1925", "2092", "3621", "25528", "10289")
+  # input = c("1103", "1104", "13575", "25528", "10289")
   uniprot_ids = c("CHK1_HUMAN", "CLH1_HUMAN", "FCGRN_HUMAN", "NECP2_HUMAN", "RFA1_HUMAN")
+  gene_id_map = hashmap()
+  gene_id_map[c("1925", "2092", "3621", "25528", "10289")] = 
+      c("CHEK1", "CLTC", "FCGRT", "NECAP2", "RPA1")
   url = "https://discovery.indra.bio/api/indra_subnetwork_relations"
   hgnc_ids = lapply(input, function(x) list("HGNC", x))
   groundings = list(nodes = hgnc_ids)
@@ -21,11 +26,15 @@ visualizeNetworks = function(input) {
   res = POST(url, body = json_body, add_headers("Content-Type" = "application/json"), encode = "raw")
   output = content(res)
   output = Filter(function(x) x$data$stmt_type %in% c("Complex", "Activation", "Inhibition"), output)
+  # NEXT STEP: COLLAPSE EDGES WITH SAME SOURCE, TARGET, and INTERACTION AND SUM EVIDENCE COUNT - HELPER FUNCTION
+  evidenceList = sapply(output, function(x) 
+      paste("https://db.indra.bio/statements/from_agents?subject=", 
+            query(gene_id_map, x$source_id), "&object=", 
+            query(gene_id_map, x$target_id), "&type=",
+            x$data$stmt_type, "&format=html", sep=""))
   
   
   # Construct Cytoscape network
-  # NEXT STEP: CREATE EVIDENCE LINK TO INDRA DB WEBSITE HTML ASSEMBLER
-  # NEXT STEP: COLLAPSE EDGES WITH SAME SOURCE AND TARGET
   nodes = data.frame(id=input,
                      uniprot_id=uniprot_ids,
                       logFC=c(0.1,-0.3,-0.7,0.7,-0.6), 
@@ -36,11 +45,11 @@ visualizeNetworks = function(input) {
                       interaction=sapply(output, function(x) x$data$stmt_type), 
                       correlation=c(0.2,0.3,0.4,0.4),
                       evidenceCount=sapply(output, function(x) x$data$evidence_count),
-                      evidenceList=sapply(output, function(x) jsonlite::toJSON(jsonlite::fromJSON(x$data$stmt_json)$evidence)),
+                      evidenceLink=evidenceList,
                       stringsAsFactors=FALSE)
   
   
-  createNetworkFromDataFrames(nodes,edges, title="my first network", collection="DataFrame Example")
+  network_id = createNetworkFromDataFrames(nodes,edges, title="my first network", collection="DataFrame Example")
   arrowShapes = mapVisualProperty(
       'Edge Target Arrow Shape','interaction',
       'd',
