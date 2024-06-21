0ea7bd89d85cc963533f30b0028081dc71621412
Tony Wu <ant.wu@northeastern.edu>
2024-06-21 09:53:46 -0400

    split into 2 functions

--- R/networkPlots.R
+++ R/networkPlots.R
@@ -1,63 +1,78 @@
 
-#' Create visualization of networks in plotly
+#' Fetch data on a set of proteins from INDRA
 #' 
-#' @param input list of hgnc IDs
+#' @param input groupComparison comparisionResult table
+#' @param pvalue_cutoff p-value cutoff for filtering
+#' @param stmt_types types of statements to filter, default is "Complex"
 #' @importFrom jsonlite toJSON
-#' @importFrom httr POST, add_headers
-#' @importFrom r2r hashmap, query
+#' @importFrom httr POST add_headers content
+#' @importFrom r2r hashmap query keys
+#' @importFrom dplyr filter
 #' 
 #' @export
 #' 
-#' @NoRd
+#' @noRd
+#'
+#'   
+fetchIndraData = function(input, pvalue_cutoff = 0.05, stmt_types = c("Complex")) {
+    input = filter(input, adj.pvalue < pvalue_cutoff)
+    input = filter(input, is.na(issue))
+    hgnc_ids = input$HgncId
+    uniprot_ids = input$Protein
+    gene_id_map = hashmap()
+    gene_id_map[input$HgncId] = input$HgncName
+    url = "https://discovery.indra.bio/api/indra_subnetwork_relations"
+    groundings = lapply(hgnc_ids, function(x) list("HGNC", x))
+    groundings = list(nodes = groundings)
+    json_body = jsonlite::toJSON(groundings, auto_unbox = TRUE)
+    res = POST(url, body = json_body, add_headers("Content-Type" = "application/json"), encode = "raw")
+    output = content(res)
+    output = Filter(function(x) x$data$stmt_type %in% stmt_types, output)
+    
+    edge_data = hashmap()
+    for (edge in output) {
+        key = paste(edge$source_id, edge$target_id, edge$data$stmt_type, sep="_")
+        if (key %in% keys(edge_data)) {
+            edge_data[[key]]$data$evidence_count = edge_data[[key]]$data$evidence_count + edge$data$evidence_count
+        } else {
+            edge_data[[key]] = edge
+        }
+    }
+    evidenceList = sapply(keys(edge_data), function(x) 
+        paste("https://db.indra.bio/statements/from_agents?subject=", 
+              query(gene_id_map, query(edge_data, x)$source_id), "&object=", 
+              query(gene_id_map, query(edge_data, x)$target_id), "&type=",
+              query(edge_data, x)$data$stmt_type, "&format=html", sep=""))
+    
+    
+    nodes = data.frame(id=hgnc_ids,
+                       uniprot_id=uniprot_ids,
+                       logFC=input$log2FC, 
+                       pvalue=input$adj.pvalue,
+                       stringsAsFactors=FALSE)
+    edges = data.frame(source=sapply(keys(edge_data), function(x) query(edge_data, x)$source_id),
+                       target=sapply(keys(edge_data), function(x) query(edge_data, x)$target_id),
+                       interaction=sapply(keys(edge_data), function(x) query(edge_data, x)$data$stmt_type), 
+                       evidenceCount=sapply(keys(edge_data), function(x) query(edge_data, x)$data$evidence_count),
+                       evidenceLink=evidenceList,
+                       stringsAsFactors=FALSE)
+    
+    return(list(nodes=nodes, edges=edges))
+}
+
+#' Create visualization of networks in cytoscape
+#' 
+#' @param nodes dataframe of nodes
+#' @param edges dataframe of edges
+#' @importFrom RCy3 createNetworkFromDataFrames mapVisualProperty createVisualStyle setVisualStyle
+#' 
+#' @export
+#' 
+#' @noRd
 #'
 #'               
-visualizeNetworks = function(input) {
-  # FUNCTION 1 - Fetch data from INDRA and return nodes and edges dataframes
-  input = c("1925", "2092", "3621", "25528", "10289")
-  input = c("1103", "1104", "13575", "25528", "10289")
-  uniprot_ids = c("BRD2_HUMAN", "BRD3_HUMAN", "BRD4_HUMAN", "NECP2_HUMAN", "RFA1_HUMAN")
-  gene_id_map = hashmap()
-  gene_id_map[c("1103", "1104", "13575", "25528", "10289")] = 
-      c("BRD2", "BRD3", "BRD4", "NECAP2", "RPA1")
-  url = "https://discovery.indra.bio/api/indra_subnetwork_relations"
-  hgnc_ids = lapply(input, function(x) list("HGNC", x))
-  groundings = list(nodes = hgnc_ids)
-  json_body = jsonlite::toJSON(groundings, auto_unbox = TRUE)
-  res = POST(url, body = json_body, add_headers("Content-Type" = "application/json"), encode = "raw")
-  output = content(res)
-  output = Filter(function(x) x$data$stmt_type %in% c("Complex", "Activation", "Inhibition"), output)
-
-  edge_data = hashmap()
-  for (edge in output) {
-    key = paste(edge$source_id, edge$target_id, edge$data$stmt_type, sep="_")
-    if (key %in% keys(edge_data)) {
-      edge_data[[key]]$data$evidence_count = edge_data[[key]]$data$evidence_count + edge$data$evidence_count
-    } else {
-      edge_data[[key]] = edge
-    }
-  }
-  evidenceList = sapply(keys(edge_data), function(x) 
-      paste("https://db.indra.bio/statements/from_agents?subject=", 
-            query(gene_id_map, query(edge_data, x)$source_id), "&object=", 
-            query(gene_id_map, query(edge_data, x)$target_id), "&type=",
-            query(edge_data, x)$data$stmt_type, "&format=html", sep=""))
-  
-
-  nodes = data.frame(id=input,
-                     uniprot_id=uniprot_ids,
-                      logFC=c(0.1,-0.3,-0.7,0.7,-0.6), 
-                      pvalue=c(0.99, 0.003, 0.001, 0.00000002, 0.0000000003),
-                      stringsAsFactors=FALSE)
-  edges = data.frame(source=sapply(keys(edge_data), function(x) query(edge_data, x)$source_id),
-                      target=sapply(keys(edge_data), function(x) query(edge_data, x)$target_id),
-                      interaction=sapply(keys(edge_data), function(x) query(edge_data, x)$data$stmt_type), 
-                      # correlation=c(0.2,0.3,0.4,0.4),
-                      evidenceCount=sapply(keys(edge_data), function(x) query(edge_data, x)$data$evidence_count),
-                      evidenceLink=evidenceList,
-                      stringsAsFactors=FALSE)
-  
-  # FUNCTION 2 - Create network from node and edge dataframes
-  network_id = createNetworkFromDataFrames(nodes,edges, title="my first network", collection="DataFrame Example")
+visualizeNetworks = function(nodes, edges) {
+  network_id = createNetworkFromDataFrames(nodes, edges, title="my first network", collection="DataFrame Example")
   arrowShapes = mapVisualProperty(
       'Edge Target Arrow Shape','interaction',
       'd',
