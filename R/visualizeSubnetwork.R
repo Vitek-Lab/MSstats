8659c56e5e2089a6e1c119bdf4e6b9c27fea204e
Tony Wu <ant.wu@northeastern.edu>
2024-07-16 10:23:34 -0400

    trying to add logFC coloring

--- R/visualizeSubnetwork.R
+++ R/visualizeSubnetwork.R
@@ -2,7 +2,9 @@
 #' 
 #' @param nodes dataframe of nodes
 #' @param edges dataframe of edges
-#' @importFrom RCy3 createNetworkFromDataFrames mapVisualProperty createVisualStyle setVisualStyle
+#' @param pvalue_cutoff p-value cutoff for highlighting significant proteins. Default is 0.05
+#' @param logfc_cutoff log fold change cutoff for highlighting significant proteins. Default is 0.5
+#' @importFrom RCy3 createNetworkFromDataFrames mapVisualProperty createVisualStyle setVisualStyle setNodeColorMapping
 #' 
 #' @export
 #' 
@@ -13,9 +15,9 @@
 #' # visualizeSubnetwork(subnetwork$nodes, subnetwork$edges)
 #'
 #'               
-visualizeSubnetwork = function(nodes, edges) {
+visualizeSubnetwork = function(nodes, edges, pvalue_cutoff = 0.05, logfc_cutoff = 0.5) {
     DEFAULT_VISUAL_STYLE = list(
-        NODE_FILL_COLOR="lightblue",
+        # NODE_FILL_COLOR="lightblue",
         NODE_SHAPE="ROUNDRECT",
         NODE_SIZE=50,
         NODE_LABEL_FONT_SIZE=6,
@@ -24,6 +26,9 @@ visualizeSubnetwork = function(nodes, edges) {
     VISUAL_STYLE_NAME = "MSstats-Indra Visual Style"
     
     createNetworkFromDataFrames(nodes, edges)
+    control.points <- c (-logfc_cutoff, 0.0, logfc_cutoff)
+    colors <-  c ('#CC5522',  '#DD8855', '#FFFFFF','#5588DD','#2255CC')
+    RCy3::setNodeColorMapping("logFC", control.points, colors)
     
     VISUAL_STYLE_MAPPINGS = list(
         mapVisualProperty('Node Label','id','p')
