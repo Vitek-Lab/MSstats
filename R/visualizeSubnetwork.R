85241a09d7424827974f35e5dde03f51a7eb961e
Tony Wu <ant.wu@northeastern.edu>
2024-07-16 14:16:05 -0400

    refactor visualization code

--- R/visualizeSubnetwork.R
+++ R/visualizeSubnetwork.R
@@ -2,8 +2,8 @@
 #' 
 #' @param nodes dataframe of nodes
 #' @param edges dataframe of edges
-#' @param pvalue_cutoff p-value cutoff for highlighting significant proteins. Default is 0.05
-#' @param logfc_cutoff log fold change cutoff for highlighting significant proteins. Default is 0.5
+#' @param pvalue_cutoff p-value cutoff for coloring significant proteins. Default is 0.05
+#' @param logfc_cutoff log fold change cutoff for coloring significant proteins. Default is 0.5
 #' @importFrom RCy3 createNetworkFromDataFrames mapVisualProperty createVisualStyle setVisualStyle
 #' 
 #' @export
@@ -15,9 +15,19 @@
 #' # visualizeSubnetwork(subnetwork$nodes, subnetwork$edges)
 #'
 #'               
-visualizeSubnetwork = function(nodes, edges, pvalue_cutoff = 0.05, logfc_cutoff = 0.5) {
+visualizeSubnetwork = function(nodes, edges, 
+                               pvalue_cutoff = 0.05, logfc_cutoff = 0.5) {
+    
+    # Add additional columns for visualization
+    nodes$logFC_color = nodes$logFC
+    nodes$logFC_color[nodes$pvalue > pvalue_cutoff | 
+                          abs(nodes$logFC) < logfc_cutoff] = 0
+    
+    # Create network
+    createNetworkFromDataFrames(nodes, edges)
+    
+    # Apply visual style
     DEFAULT_VISUAL_STYLE = list(
-        # NODE_FILL_COLOR="lightblue",
         NODE_SHAPE="ROUNDRECT",
         NODE_SIZE=50,
         NODE_LABEL_FONT_SIZE=6,
@@ -25,16 +35,11 @@ visualizeSubnetwork = function(nodes, edges, pvalue_cutoff = 0.05, logfc_cutoff 
         EDGE_TARGET_ARROW_SHAPE="Arrow")
     VISUAL_STYLE_NAME = "MSstats-Indra Visual Style"
     
-    nodes$logFC_color = nodes$logFC
-    nodes$logFC_color[nodes$pvalue > pvalue_cutoff | abs(nodes$logFC) < logfc_cutoff] = 0
-    
-    createNetworkFromDataFrames(nodes, edges)
-    
     VISUAL_STYLE_MAPPINGS = list(
         mapVisualProperty('Node Label','id','p'),
         mapVisualProperty('Node Fill Color','logFC_color','c',
                           c(-logfc_cutoff, 0.0, logfc_cutoff),
-                          c('#CC5522','#DD8855','#FFFFFF','#5588DD','#2255CC'))
+                          c('#5588DD','#5588DD','#D3D3D3','#DD8855','#DD8855'))
         
     )
     createVisualStyle(VISUAL_STYLE_NAME, DEFAULT_VISUAL_STYLE, VISUAL_STYLE_MAPPINGS)
