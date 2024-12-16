8a6c6bacc51cfdaae6790888c4a9e39c24d143bc
Tony Wu <wu.anthon@northeastern.edu>
2024-12-16 09:39:31 -0500

    refactor(converters): Reexport converters to MSstats namespace

---
+++ R/converters.R
@@ -0,0 +1,42 @@
+# For backwards compatibility, MSstats will export some converters in the 
+# MSstats namespace
+
+#' @export
+#' @importFrom MSstatsConvert DIANNtoMSstatsFormat
+MSstatsConvert::DIANNtoMSstatsFormat
+
+#' @export
+#' @importFrom MSstatsConvert DIAUmpiretoMSstatsFormat
+MSstatsConvert::DIAUmpiretoMSstatsFormat
+
+#' @export
+#' @importFrom MSstatsConvert FragPipetoMSstatsFormat
+MSstatsConvert::FragPipetoMSstatsFormat
+
+#' @export
+#' @importFrom MSstatsConvert MaxQtoMSstatsFormat
+MSstatsConvert::MaxQtoMSstatsFormat
+
+#' @export
+#' @importFrom MSstatsConvert OpenMStoMSstatsFormat
+MSstatsConvert::OpenMStoMSstatsFormat
+
+#' @export
+#' @importFrom MSstatsConvert OpenSWATHtoMSstatsFormat
+MSstatsConvert::OpenSWATHtoMSstatsFormat
+
+#' @export
+#' @importFrom MSstatsConvert PDtoMSstatsFormat
+MSstatsConvert::PDtoMSstatsFormat
+
+#' @export
+#' @importFrom MSstatsConvert ProgenesistoMSstatsFormat
+MSstatsConvert::ProgenesistoMSstatsFormat
+
+#' @export
+#' @importFrom MSstatsConvert SkylinetoMSstatsFormat
+MSstatsConvert::SkylinetoMSstatsFormat
+
+#' @export
+#' @importFrom MSstatsConvert SpectronauttoMSstatsFormat
+MSstatsConvert::SpectronauttoMSstatsFormat
