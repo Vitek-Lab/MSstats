6df52e6eaa2aaadbcf7b2eb0c79435de1f0b38e2
tonywu1999 <wu.anthon@northeastern.edu>
2025-01-09 10:55:29 -0500

    refactor(converters): Load MSstatsConvert converters when calling library(MSstats) (#146)
    * refactor(converters): Load MSstatsConvert converters when calling library(MSstats)
    
    * refactor(converters): Reexport converters to MSstats namespace

---
+++ R/utils_documentation.R
@@ -0,0 +1,30 @@
+#' A dummy function to store shared documentation items.
+#' 
+#' @import data.table
+#' @importFrom MSstatsConvert MSstatsImport MSstatsClean MSstatsPreprocess 
+#' MSstatsBalancedDesign MSstatsMakeAnnotation MSstatsLogsSettings
+#' 
+#' @param removeFewMeasurements TRUE (default) will remove the features that have 1 or 2 measurements across runs.
+#' @param useUniquePeptide TRUE (default) removes peptides that are assigned for more than one proteins. 
+#' We assume to use unique peptide for each protein.
+#' @param summaryforMultipleRows max(default) or sum - when there are multiple measurements for certain feature and certain run, use highest or sum of multiple intensities.
+#' @param removeProtein_with1Feature TRUE will remove the proteins which have only 1 feature, which is the combination of peptide, precursor charge, fragment and charge. FALSE is default.
+#' @param removeProtein_with1Peptide TRUE will remove the proteins which have only 1 peptide and charge. FALSE is default.
+#' @param removeOxidationMpeptides TRUE will remove the peptides including 'oxidation (M)' in modification. FALSE is default.
+#' @param removeMpeptides TRUE will remove the peptides including 'M' sequence. FALSE is default.
+#' @param use_log_file logical. If TRUE, information about data processing
+#' will be saved to a file.
+#' @param append logical. If TRUE, information about data processing will be added
+#' to an existing log file.
+#' @param verbose logical. If TRUE, information about data processing wil be printed
+#' to the console.
+#' @param log_file_path character. Path to a file to which information about 
+#' data processing will be saved. 
+#' If not provided, such a file will be created automatically.
+#' If `append = TRUE`, has to be a valid path to a file.
+#' 
+#' @keywords internal
+#' 
+.documentFunction = function() {
+    NULL
+}
