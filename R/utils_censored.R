1265fbc94c7764f1c0643448656a1539ed8a9a3f
Tony Wu <wu.anthon@northeastern.edu>
2025-06-27 13:24:29 -0400

    trying new approach

--- R/utils_censored.R
+++ R/utils_censored.R
@@ -99,14 +99,13 @@ MSstatsHandleMissing = function(input, summary_method, impute,
   
   input[, nonmissing_all := ifelse(total_features > 1 & n_obs <= 1, 
                                    FALSE, nonmissing_all)] 
-  grouping_vars = c("PROTEIN", "FEATURE", "LABEL")
-  subset_input = input[n_obs > 1 & n_obs_run > 0 & nonmissing_all, 
-                       .(PROTEIN, FEATURE, LABEL, newABUNDANCE, nonmissing_all)]
+  subset_input = input[n_obs > 1 & n_obs_run > 0 & nonmissing_all,
+                       .(PROTEIN, FEATURE, LABEL, newABUNDANCE)]
   grouped = subset_input[, .(
       MinValue = min(newABUNDANCE, na.rm = TRUE)
   ), by = .(PROTEIN, FEATURE, LABEL)]
   grouped[, NinetyNine := 0.99 * MinValue]
-  input[grouped, ABUNDANCE_cut := NinetyNine, 
+  input[grouped, ABUNDANCE_cut := NinetyNine,
         on = c("PROTEIN", "FEATURE", "LABEL")]
   input[, any_censored := any(censored & n_obs > 1 & n_obs_run > 0),
         by = "PROTEIN"]
