8c06d9dea52bcdff561c1d9b8812affa1055d532
tonywu1999 <wu.anthon@northeastern.edu>
2025-06-30 15:43:07 -0400

    add starting point for test case

--- inst/tinytest/test_utils_censored.R
+++ inst/tinytest/test_utils_censored.R
@@ -1,15 +1,24 @@
 # Test .setCensoredByThreshold
+library(data.table)
+dt_na[, nonmissing := MSstats:::.getNonMissingFilter(dt_na, TRUE, "NA")]
+dt_na[, n_obs := sum(nonmissing), by = c("PROTEIN", "FEATURE")]
+dt_na[, nonmissing := ifelse(n_obs <= 1, FALSE, nonmissing)] 
+dt_na[, n_obs_run := sum(nonmissing), by = c("PROTEIN", "RUN")]
+
+dt_na[, total_features := uniqueN(FEATURE), by = "PROTEIN"]
+dt_na[, prop_features := sum(nonmissing) / total_features,
+      by = c("PROTEIN", "RUN")] 
 dt_na <- data.table::data.table(
   PROTEIN = c("P1", "P1", "P1", "P1", "P1", "P1", "P1", "P1", "P1", "P1", "P1", "P1", 
               "P2", "P2", "P2", "P2", "P2", "P2", "P2", "P2", "P2", "P2", "P2", "P2"),
   FEATURE = c("F1", "F1", "F2", "F2", "F1", "F2", "F1", "F1", "F2", "F2", "F1", "F2", 
-              "F1", "F1", "F2", "F2", "F1", "F2", "F1", "F1", "F2", "F2", "F1", "F2"),
+              "F3", "F3", "F4", "F4", "F3", "F4", "F3", "F3", "F4", "F4", "F3", "F4"),
   LABEL = c("L", "L", "L", "L", "H", "H", "L", "L", "L", "L", "H", "H", 
             "L", "L", "L", "L", "H", "H", "L", "L", "L", "L", "H", "H"),
   RUN = c("R1", "R2", "R1", "R2", "R1", "R2", "R3", "R4", "R3", "R4", "R3", "R4", 
           "R1", "R2", "R1", "R2", "R1", "R2", "R3", "R4", "R3", "R4", "R3", "R4"),
   newABUNDANCE = c(1.5, NA, 2.0, 2.2, 1.4, 1.6, 1.5, 1.9, 2.0, 2.2, 1.4, 1.6, 
-                   0.0, 4.0, 2.5, 2.7, 3.0, 3.2, 1.7, 4.0, 2.5, 2.7, 3.0, 3.2),
+                   0.0, 4.0, 2.5, 2.7, 3.0, 3.2, NA, 4.0, NA, 2.7, 3.0, 3.2),
   censored = c(FALSE, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, 
                TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE),
   nonmissing = c(TRUE, FALSE, TRUE, TRUE, FALSE, FALSE, TRUE, TRUE, TRUE, TRUE, FALSE, FALSE, 
