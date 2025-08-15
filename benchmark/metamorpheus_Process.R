7485d9519545cbed3c56d885a66d3d4709b25f1d
Anshuman Raina <raina.ans@northeastern.edu>
2025-08-15 15:58:48 -0400

    Changes for variable name correction

--- benchmark/metamorpheus_Process.R
+++ benchmark/metamorpheus_Process.R
@@ -20,13 +20,13 @@ calculate_Metrics <- function(QuantData, protein_mappings, task_label, alpha = 0
     filter(Organism == "Escherichia coli (strain K12)") %>%
     pull(`Protein Groups`)
 
-  comp <- model$ComparisonResult %>%
+  filtered_results <- model$ComparisonResult %>%
     mutate(ecoli = Protein %in% ecoli_ids) %>%
     filter(is.na(issue))
 
-  labels <- unique(comp$Label)
+  labels <- unique(filtered_results$Label)
   result_rows <- lapply(labels, function(lbl) {
-    df <- comp %>% filter(Label == lbl)
+    df <- filtered_results %>% filter(Label == lbl)
     sig <- df %>% filter(adj.pvalue < alpha)
 
     tp <- sig %>% filter(ecoli) %>% nrow()
