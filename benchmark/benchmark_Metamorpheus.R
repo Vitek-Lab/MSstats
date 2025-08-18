90cf018acd2c26c6a2db72ed01d81070cbc2c3f6
Anshuman Raina <raina.ans@northeastern.edu>
2025-08-18 15:01:31 -0400

    Removed params

--- benchmark/benchmark_Metamorpheus.R
+++ benchmark/benchmark_Metamorpheus.R
@@ -36,7 +36,7 @@ runBenchmarkForMetaMorpheusData <- function(datasetPath, config) {
 
   input = input %>% filter(`Protein Group` %in% protein_mappings$`Protein Groups`)
 
-  output = MetamorpheusToMSstatsFormat(input, annot, removeFewMeasurements = FALSE, removeProtein_with1Feature = FALSE)
+  output = MetamorpheusToMSstatsFormat(input, annot)
 
   data_process_tasks <- list(
     list(
