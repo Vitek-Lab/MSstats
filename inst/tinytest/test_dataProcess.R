c1403628766ed6787880e3baaa6a94ee8f1fc540
Tony Wu <wu.anthon@northeastern.edu>
2025-09-08 11:58:30 -0400

    Add more granular unit tests for data summarization subfunctions

--- inst/tinytest/test_dataProcess.R
+++ inst/tinytest/test_dataProcess.R
@@ -112,3 +112,99 @@ QuantDataNoAnomaly = dataProcess(msstats_input_no_anomaly,
 no_anomaly_variance = QuantDataNoAnomaly$ProteinLevelData$Variance
 expect_false(identical(high_anomaly_variance, no_anomaly_variance),
              info = "Variance should be different when AnomalyScores are present vs absent")
+
+
+
+# Test MSstatsSummarizeSingleLinear function ------------------------------
+
+single_protein = data.table::data.table(
+    PROTEIN = c( "Q96S19", "Q96S19", "Q96S19", "Q96S19", "Q96S19", "Q96S19", "Q96S19", "Q96S19", "Q96S19", "Q96S19", "Q96S19", "Q96S19", "Q96S19", "Q96S19", "Q96S19" ),
+    PEPTIDE = c( "AFPLAEWQPSDVDQR_2", "ASGLLLER_2", "LowAbundancePeptide_2", "AFPLAEWQPSDVDQR_2", "ASGLLLER_2", "LowAbundancePeptide_2", "AFPLAEWQPSDVDQR_2", "ASGLLLER_2", "LowAbundancePeptide_2", "AFPLAEWQPSDVDQR_2", "ASGLLLER_2", "LowAbundancePeptide_2", "AFPLAEWQPSDVDQR_2", "ASGLLLER_2", "LowAbundancePeptide_2" ),
+    TRANSITION = c( "y3_1", "y3_1", "y3_1", "y3_1", "y3_1", "y3_1", "y3_1", "y3_1", "y3_1", "y3_1", "y3_1", "y3_1", "y3_1", "y3_1", "y3_1" ),
+    FEATURE = c( "AFPLAEWQPSDVDQR_2_y3_1", "ASGLLLER_2_y3_1", "LowAbundancePeptide_2_y3_1", "AFPLAEWQPSDVDQR_2_y3_1", "ASGLLLER_2_y3_1", "LowAbundancePeptide_2_y3_1", "AFPLAEWQPSDVDQR_2_y3_1", "ASGLLLER_2_y3_1", "LowAbundancePeptide_2_y3_1", "AFPLAEWQPSDVDQR_2_y3_1", "ASGLLLER_2_y3_1", "LowAbundancePeptide_2_y3_1", "AFPLAEWQPSDVDQR_2_y3_1", "ASGLLLER_2_y3_1", "LowAbundancePeptide_2_y3_1" ),
+    LABEL = c( "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L", "L" ),
+    GROUP_ORIGINAL = c( "A", "A", "A", "A", "A", "A", "A", "A", "A", "B", "B", "B", "B", "B", "B" ),
+    SUBJECT_ORIGINAL = c( 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5 ),
+    RUN = c( "1", "1", "1", "2", "2", "2", "3", "3", "3", "4", "4", "4", "5", "5", "5" ),
+    GROUP = c( "A", "A", "A", "A", "A", "A", "A", "A", "A", "B", "B", "B", "B", "B", "B" ),
+    SUBJECT = c( "1", "1", "1", "2", "2", "2", "3", "3", "3", "4", "4", "4", "5", "5", "5" ),
+    FRACTION = c( 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 ),
+    INTENSITY = c( 1200, 1300, 100, 1700, 1800, 200, 2200, 1200, 300, 2000, 2800, 400, 1700, 1800, 500 ),
+    ANOMALYSCORES = c( 0.01, 0.01, 0.1, 0.01, 0.01, 0.1, 0.01, 0.01, 0.1, 0.9, 0.7, 0.1, 0.8, 0.5, 0.1 ),
+    ABUNDANCE = c( 10.229, 10.344, 6.644, 10.731, 10.814, 7.644, 11.103, 10.229, 8.229, 10.966, 11.451, 8.644, 10.731, 10.814, 8.966 ),
+    originalRUN = c( "Run1", "Run1", "Run1", "Run2", "Run2", "Run2", "Run3", "Run3", "Run3", "Run4", "Run4", "Run4", "Run5", "Run5", "Run5" ),
+    censored = c( FALSE, FALSE, TRUE, FALSE, FALSE, TRUE, FALSE, FALSE, TRUE, FALSE, FALSE, TRUE, FALSE, FALSE, TRUE ),
+    newABUNDANCE = c( 10.229, 10.344, NA, 10.731, 10.814, NA, 11.103, 10.229, NA, 10.966, 11.451, NA, 10.731, 10.814, NA ),
+    cen = c( 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0 ),
+    nonmissing = c( TRUE, TRUE, FALSE, TRUE, TRUE, FALSE, TRUE, TRUE, FALSE, TRUE, TRUE, FALSE, TRUE, TRUE, FALSE ),
+    n_obs = c( 5, 5, 0, 5, 5, 0, 5, 5, 0, 5, 5, 0, 5, 5, 0 ),
+    n_obs_run = c( 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 ),
+    total_features = c( 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3 ),
+    prop_features = c( 0.667, 0.667, 0.667, 0.667, 0.667, 0.667, 0.667, 0.667, 0.667, 0.667, 0.667, 0.667, 0.667, 0.667, 0.667 ),
+    nonmissing_all = c( TRUE, TRUE, FALSE, TRUE, TRUE, FALSE, TRUE, TRUE, FALSE, TRUE, TRUE, FALSE, TRUE, TRUE, FALSE ),
+    ABUNDANCE_cut = c( 10.127, 10.127, NA, 10.127, 10.127, NA, 10.127, 10.127, NA, 10.127, 10.127, NA, 10.127, 10.127, NA ),
+    any_censored = c( FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE )
+)
+
+summarized = MSstats::MSstatsSummarizeSingleLinear(
+    single_protein,
+    impute = FALSE,
+    censored_symbol = "NA", 
+    remove50missing = FALSE, 
+    aft_iterations = 90,
+    equal_variances = TRUE
+)
+
+protein_level_summary = summarized[[1]]
+feature_level_summary = summarized[[2]]
+
+## Basic tests
+expect_equal(nrow(protein_level_summary), 5)
+expect_equal(ncol(protein_level_summary), 4)
+expect_true(all(protein_level_summary$Protein == "Q96S19"))
+expect_equal(as.numeric(protein_level_summary$RUN), 1:5)
+expect_equal(nrow(feature_level_summary), 10)
+expect_equal(ncol(feature_level_summary), 5)
+expect_equal(length(unique(feature_level_summary$FEATURE)), 2)
+expect_equal(length(unique(feature_level_summary$RUN)), 5)
+
+## Verify that variance is higher in runs 4-5 with high anomaly scores
+variance_runs_1_3 = protein_level_summary[RUN %in% 1:3, Variance]
+variance_runs_4_5 = protein_level_summary[RUN %in% 4:5, Variance]
+
+for (var_45 in variance_runs_4_5) {
+    for (var_13 in variance_runs_1_3) {
+        expect_true(var_45 > var_13, 
+                  info = paste("Variance in runs 4-5 should be > variance in runs 1-3:",
+                               var_45, ">", var_13))
+    }
+}
+
+mean_variance_1_3 = mean(variance_runs_1_3)
+mean_variance_4_5 = mean(variance_runs_4_5)
+
+expect_true(mean_variance_4_5 > 10 * mean_variance_1_3,
+          info = paste("Mean variance of runs 4-5 should be at least 10x higher:",
+                       mean_variance_4_5, "vs", 10 * mean_variance_1_3))
+
+## Verify that protein levels in runs 4-5 are closer to ASGLLLER than AFPLAEWQPSDVDQR
+protein_runs_4_5 = protein_level_summary[RUN %in% 4:5]
+afpla_runs_4_5 = feature_level_summary[FEATURE == "AFPLAEWQPSDVDQR_2_y3_1" & RUN %in% 4:5]
+asgll_runs_4_5 = feature_level_summary[FEATURE == "ASGLLLER_2_y3_1" & RUN %in% 4:5]
+for (i in 1:nrow(protein_runs_4_5)) {
+    run_num = protein_runs_4_5$RUN[i]
+    protein_logint = protein_runs_4_5$LogIntensities[i]
+    
+    afpla_abundance = afpla_runs_4_5[RUN == run_num, newABUNDANCE]
+    asgll_abundance = asgll_runs_4_5[RUN == run_num, newABUNDANCE]
+    
+    dist_to_afpla = abs(protein_logint - afpla_abundance)
+    dist_to_asgll = abs(protein_logint - asgll_abundance)
+    
+    expect_true(dist_to_asgll < dist_to_afpla,
+              info = paste("Run", run_num, ": Protein LogIntensity", protein_logint,
+                           "should be closer to ASGLLLER", asgll_abundance,
+                           "(dist =", round(dist_to_asgll, 4), ")",
+                           "than to AFPLAEWQPSDVDQR", afpla_abundance,
+                           "(dist =", round(dist_to_afpla, 4), ")"))
+}
