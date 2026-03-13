eb64e4f38a62af2e4c293195154d820892ea1eaa
Devon Kohler <35807256+devonjkohler@users.noreply.github.com>
2026-03-13 13:27:56 -0400

    Fix weight calculation to avoid division by zero

--- R/utils_groupcomparison.R
+++ R/utils_groupcomparison.R
@@ -140,7 +140,7 @@ getSamplesInfo = function(summarization_output) {
                                        is_single_subject, has_tech_replicates) {
     
     if ("Variance" %in% colnames(input) & !all(is.na(input$Variance))){
-        weight_input = 1 / pmax(Variance, 0.01)
+        weight_input = 1 / pmax(input$Variance, 0.001)
     } else {
         weight_input = NULL
     }
